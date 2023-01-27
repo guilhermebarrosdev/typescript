cb9af0782d0cd5a23c67a6b3d8feb1e102c606b7
Guilherme Barros <wingui3@gmail.com>
2023-01-27 05:09:22 -0300

    :sparkles: Adiciona arquivos aula string-number-boolean

---
+++ 02-typescript-basico/0202-string-number-e-boolean/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const frase1 = new String('Front End');
+const frase2 = String('Front End');
+const frase3 = 'Front End';
+console.log(typeof frase1);
+console.log(typeof frase2);
+console.log(typeof frase3.toLowerCase);
