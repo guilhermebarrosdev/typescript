3448289a6b37b1a25ff62559c7ee6b9474b5adb7
Guilherme Barros <wingui3@gmail.com>
2023-01-31 23:41:09 -0300

    :sparkles: Adiciona arquivos aula arrays

---
+++ 02-typescript-basico/0205-arrays/exercicio.ts
@@ -0,0 +1,40 @@
+async function fetchCursos() {
+  const response = await fetch('https://api.origamid.dev/json/cursos.json');
+  const data = await response.json();
+  console.log(data);
+  mostrarCursos(data);
+}
+
+fetchCursos();
+
+interface Curso {
+  nome: string;
+  aulas: number;
+  gratuito: boolean;
+  horas: number;
+  idAulas: number[];
+  tags: string[];
+  nivel: 'iniciante' | 'avancado';
+}
+
+function mostrarCursos(cursos: Curso[]) {
+  cursos.forEach((curso) => {
+    let color;
+    if (curso.nivel === 'iniciante') {
+      color = 'blue';
+    } else if (curso.nivel === 'avancado') {
+      color = 'red';
+    }
+
+    document.body.innerHTML += `
+      <div>
+        <h2 style="color: ${color};">${curso.nome}</h2>
+        <p>Horas: ${curso.horas}</p>
+        <p>Aulas: ${curso.aulas}</p>
+        <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+        <p>Tags: ${curso.tags.join(', ')}</>
+        <p>Aulas: ${curso.idAulas.join(' | ')}</>
+      </div>
+    `;
+  });
+}
