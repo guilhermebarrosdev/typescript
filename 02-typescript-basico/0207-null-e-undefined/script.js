2f2fcff2ab5ab53dcddce548c630dc9ed407ff19
Guilherme Barros <wingui3@gmail.com>
2023-02-04 16:41:05 -0300

    :sparkles: Adiciona arquivos aula null e undefined

---
+++ 02-typescript-basico/0207-null-e-undefined/script.js
@@ -0,0 +1,20 @@
+'use strict';
+const button = document.querySelector('button');
+const config = localStorage.getItem('config');
+if (button !== null) {
+  button.click();
+}
+if (button) {
+  button.click();
+}
+button?.click();
+let total;
+function teste() {}
+const jogo = {
+  nome: 'Ragnarok',
+};
+const livro = {};
+if (jogo.nome) {
+  jogo.nome.toLowerCase();
+}
+livro.nome?.toLowerCase();
---
+++ 02-typescript-basico/0207-null-e-undefined/script.ts
@@ -0,0 +1,39 @@
+const button = document.querySelector('button');
+const config = localStorage.getItem('config');
+
+if (button !== null) {
+  button.click();
+}
+
+if (button) {
+  button.click();
+}
+
+button?.click();
+
+let total;
+
+function teste() {}
+
+// console.log(typeof total);
+
+// if (total) {
+//   console.log("Total foi definido");
+// } else {
+//   console.log("Total não foi definido");
+// }
+
+interface Product {
+  nome?: string;
+}
+
+const jogo: Product = {
+  nome: 'Ragnarok',
+};
+const livro: Product = {};
+
+if (jogo.nome) {
+  jogo.nome.toLowerCase();
+}
+
+livro.nome?.toLowerCase();
