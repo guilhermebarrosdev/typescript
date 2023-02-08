47d21898c87582c43ee81f8ee26e8664e0124914
Guilherme Barros <wingui3@gmail.com>
2023-02-08 20:20:33 -0300

    :sparkles: Adiciona arquivos aula eventos-e-callback

---
+++ 03-objetos-interfaces/0303-eventos-e-callback/exercicio.ts
@@ -0,0 +1,21 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event: PointerEvent) {
+  const nav = document.getElementById('nav');
+  const button = event.currentTarget;
+
+  if (button instanceof HTMLElement && nav) {
+    const active = nav.classList.contains('active');
+    if (active) {
+      nav.classList.remove('active');
+      button.setAttribute('aria-expanded', 'false');
+      button.setAttribute('aria-label', 'Abrir Menu');
+    } else {
+      nav.classList.add('active');
+      button.setAttribute('aria-expanded', 'true');
+      button.setAttribute('aria-label', 'Fechar Menu');
+    }
+  }
+}
+
+btnMobile?.addEventListener('pointerdown', toggleMenu);
