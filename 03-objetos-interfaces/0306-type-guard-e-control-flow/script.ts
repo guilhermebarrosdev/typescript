fc51bfd08e8fed75ad17023021dd8929af61511c
Guilherme Barros <wingui3@gmail.com>
2023-02-11 15:47:21 -0300

    :sparkles: Adiciona arquivos aula type-guard-e-control-flow

---
+++ 03-objetos-interfaces/0306-type-guard-e-control-flow/script.ts
@@ -0,0 +1,45 @@
+function typeGuard(value: any) {
+  if (typeof value === 'string') {
+    return value.toLowerCase();
+  }
+  if (typeof value === 'number') {
+    return value.toFixed();
+  }
+  if (value instanceof HTMLElement) {
+    return value.innerText;
+  }
+}
+
+typeGuard(200);
+typeGuard('333');
+typeGuard(document.body);
+
+const obj = {
+  nome: 'Origamid',
+};
+
+if ('preco' in obj) {
+  console.log('sim');
+}
+
+async function fetchProduto() {
+  const response = await fetch('https://api.origamid.dev/json/notebook.json');
+  const json = await response.json();
+  handleProduto(json);
+}
+
+interface Produto {
+  nome: string;
+  preco: number;
+}
+
+function handleProduto(data: Produto) {
+  if ('preco' in data) {
+    document.body.innerHTML += `
+      <p>Nome: ${data.nome}</p>
+      <p>Preço: R$ ${data.preco + 100}</p>
+    `;
+  }
+}
+
+fetchProduto();
