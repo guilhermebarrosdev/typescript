96d68a6707682aa7aac8c7fbedde6094229e7117
Guilherme Barros <wingui3@gmail.com>
2023-02-25 19:46:13 -0300

    :sparkles: Adiciona arquivos aula module

---
+++ 05-desenvolvimento/0502-module/src/script.ts
@@ -0,0 +1,11 @@
+import pluginSlide from './pluginSlide.js';
+import { URL_BASE, Produto } from './global.js';
+
+pluginSlide('div');
+
+console.log(URL_BASE);
+
+const livro: Produto = {
+  nome: 'str',
+  preco: 200,
+};
