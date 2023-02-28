0920e586e06c62a30c22394784153384a919c07d
Guilherme Barros <wingui3@gmail.com>
2023-02-27 23:40:26 -0300

    :sparkles: Adiciona arquivos aula bibliotecas-externas

---
+++ 05-desenvolvimento/0504-bibliotecas-externas/src/script.ts
@@ -0,0 +1,11 @@
+const body = $('body');
+body.addClass('ativo');
+
+_.intersection([1, 2, 3, 5, 6], [2, 3, 1, 9]);
+
+declare const Vimeo: any;
+
+const iframe = document.getElementById('vimeo');
+const player = new Vimeo.Player(iframe);
+
+console.log(player);
