1db755ff81329edffc95a1ce21c771a3efd02033
Guilherme Barros <wingui3@gmail.com>
2023-03-17 18:51:40 -0300

    :sparkles: Adiciona arquivos aula string-to-date

---
+++ 06-pratica-dados/0605-string-to-date/src/script.ts
@@ -0,0 +1,16 @@
+import fetchData from './fetchData.js';
+import normalizarTransacao from './normalizarTransacao.js';
+
+async function handleData() {
+  const data = await fetchData<TransacaoAPI[]>(
+    'https://api.origamid.dev/json/transacoes.json?'
+  );
+  if (!data) return;
+  const transacoes = data.map(normalizarTransacao);
+  console.log(transacoes);
+  transacoes.forEach((item) => {
+    console.log(item.data.getHours());
+  });
+}
+
+handleData();
