1f4fe106549f5de4bd357184cbba46a320713449
Guilherme Barros <wingui3@gmail.com>
2023-03-22 19:19:02 -0300

    :sparkles: Adiciona arquivos aula preencher-lista

---
+++ 06-pratica-dados/0609-preencher-lista/src/Estatisticas.ts
@@ -0,0 +1,30 @@
+import countBy from './countBy.js';
+
+type TransacaoValor = Transacao & { valor: number };
+function filtrarValor(transacao: Transacao): transacao is TransacaoValor {
+  return transacao.valor !== null;
+}
+
+export default class Estatisticas {
+  private transacoes;
+  total;
+  pagamento;
+  status;
+  constructor(transacoes: Transacao[]) {
+    this.transacoes = transacoes;
+    this.total = this.setTotal();
+    this.pagamento = this.setPagamento();
+    this.status = this.setStatus();
+  }
+  private setTotal() {
+    return this.transacoes.filter(filtrarValor).reduce((acc, item) => {
+      return acc + item.valor;
+    }, 0);
+  }
+  private setPagamento() {
+    return countBy(this.transacoes.map(({ pagamento }) => pagamento));
+  }
+  private setStatus() {
+    return countBy(this.transacoes.map(({ status }) => status));
+  }
+}
