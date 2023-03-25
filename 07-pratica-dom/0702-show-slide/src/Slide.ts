fd05cc88ac5f8262ed15a9aa01b93597b8b87d14
Guilherme Barros <wingui3@gmail.com>
2023-03-25 14:10:52 -0300

    :sparkles: Adiciona arquivos aula show-slide

---
+++ 07-pratica-dom/0702-show-slide/src/Slide.ts
@@ -0,0 +1,33 @@
+export default class Slide {
+  container: Element;
+  slides: Element[];
+  controls: Element;
+  time: number;
+  index: number;
+  slide: Element;
+  constructor(
+    container: Element,
+    slides: Element[],
+    controls: Element,
+    time: number = 5000
+  ) {
+    this.container = container;
+    this.slides = slides;
+    this.controls = controls;
+    this.time = time;
+
+    this.index = 0;
+    this.slide = this.slides[this.index];
+
+    this.show(this.index);
+  }
+  hide(el: Element) {
+    el.classList.remove('active');
+  }
+  show(index: number) {
+    this.index = index;
+    this.slide = this.slides[this.index];
+    this.slides.forEach((el) => this.hide(el));
+    this.slide.classList.add('active');
+  }
+}
