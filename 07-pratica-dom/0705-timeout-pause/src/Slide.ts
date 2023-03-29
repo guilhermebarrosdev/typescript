8bf53d9b20d8e1b29d01ee2e6571c17fda03b338
Guilherme Barros <wingui3@gmail.com>
2023-03-29 20:04:50 -0300

    :sparkles: Adiciona arquivos aula timeout-pause

---
+++ 07-pratica-dom/0705-timeout-pause/src/Slide.ts
@@ -0,0 +1,85 @@
+import Timeout from './Timeout.js';
+
+export default class Slide {
+  container: Element;
+  slides: Element[];
+  controls: Element;
+  time: number;
+  index: number;
+  slide: Element;
+  timeout: Timeout | null;
+  pausedTimeout: Timeout | null;
+  paused: boolean;
+  constructor(
+    container: Element,
+    slides: Element[],
+    controls: Element,
+    time: number = 5000
+  ) {
+    this.container = container;
+    this.slides = slides;
+    this.controls = controls;
+    this.time = time;
+    this.timeout = null;
+    this.pausedTimeout = null;
+    this.index = 0;
+    this.slide = this.slides[this.index];
+    this.paused = false;
+
+    this.init();
+  }
+  hide(el: Element) {
+    el.classList.remove('active');
+  }
+  show(index: number) {
+    this.index = index;
+    this.slide = this.slides[this.index];
+    this.slides.forEach((el) => this.hide(el));
+    this.slide.classList.add('active');
+    this.auto(this.time);
+  }
+  auto(time: number) {
+    this.timeout?.clear();
+    this.timeout = new Timeout(() => this.next(), time);
+  }
+  prev() {
+    if (this.paused) return;
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev);
+  }
+  next() {
+    if (this.paused) return;
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.show(next);
+  }
+  pause() {
+    this.pausedTimeout = new Timeout(() => {
+      this.timeout?.pause();
+      this.paused = true;
+    }, 300);
+  }
+  continue() {
+    this.pausedTimeout?.clear();
+    if (this.paused) {
+      this.paused = false;
+      this.timeout?.continue();
+    }
+  }
+  private addControls() {
+    const prevButton = document.createElement('button');
+    const nextButton = document.createElement('button');
+    prevButton.innerText = 'Slide Anterior';
+    nextButton.innerText = 'Próximo Slide';
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+
+    this.controls.addEventListener('pointerdown', () => this.pause());
+    this.controls.addEventListener('pointerup', () => this.continue());
+    prevButton.addEventListener('pointerup', () => this.prev());
+    nextButton.addEventListener('pointerup', () => this.next());
+  }
+  private init() {
+    this.addControls();
+    this.show(this.index);
+  }
+}
